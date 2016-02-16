769575b181da52dbc17f6949bc2191ec299aa811
Ivan Božić <ivan@arsfutura.co>
2016-02-16 23:33:17 +0100

    Added the favicon. Cleaned up the Gulpfile and fixed bug where the CSS would not get injected.

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,42 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var inject = require('gulp-inject');
+var clean = require('gulp-clean');
+var rev = require('gulp-rev');
 
-gulp.task('index', function () {
-	gulp.src('./index.html').pipe(gulp.dest('./public'));
-
-	var target = gulp.src('./public/index.html');
-	var sources = gulp.src(['./public/site.min.css'], { read: false });
-
-	return target.pipe(inject(sources, { relative: true })).pipe(gulp.dest('./public/'));
+gulp.task('clean', function () {
+	gulp.src('./public/**/*.*', { read: false }).pipe(clean());
 });
 
-gulp.task('moveAssets', function () {
+gulp.task('sass', ['clean'], function () {
+	return gulp.src('./assets/stylesheets/site.scss')
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}).on('error', sass.logError))
+	.pipe(rev())
+	.pipe(gulp.dest('./public'));
+});
+
+gulp.task('moveAssets', ['sass'], function () {
 	gulp.src('./assets/fonts/**/*.*')
 	.pipe(gulp.dest('./public/fonts'));
 
 	gulp.src('./assets/images/**/*.*')
 	.pipe(gulp.dest('./public/images'));
+
+	gulp.src('./assets/public/**/*.*')
+	.pipe(gulp.dest('./public'));
+
+	return gulp.src('./index.html').pipe(gulp.dest('./public'));
 });
 
-gulp.task('sass', function () {
-	return gulp.src('./assets/stylesheets/site.scss')
-    .pipe(sass({
-    	outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    .pipe(rename('site.min.css'))
-    .pipe(gulp.dest('./public'));
+gulp.task('index', ['moveAssets'], function () {
+	var target = gulp.src('./public/index.html');
+	var sources = gulp.src(['./public/**/*.css'], { read: false });
+
+	return target.pipe(inject(sources, { relative: true })).pipe(gulp.dest('./public/'));
 });
 
-gulp.task('default', ['sass', 'moveAssets', 'index'], function () {
+gulp.task('default', ['index'], function () {
 
 });
