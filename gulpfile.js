2d2037af7a3c495ba9452733b55a102f249928d2
Ivan Božić <ivan@arsfutura.co>
2016-04-26 20:02:50 +0200

    Cleaned up the Gulpfile and fixed some async issues with Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var del = require('del');
-var concat = require('gulp-concat');
+var runSequence = require('run-sequence');
+var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
@@ -10,12 +10,10 @@ var inject = require('gulp-inject');
 var rev = require('gulp-rev');
 
 gulp.task('clean', function () {
-	return del([
-		'./public/**/*'
-	]);
+	return gulp.src('./public/**/*.*', { read: false }).pipe(clean());
 });
 
-gulp.task('sass', ['clean'], function () {
+gulp.task('sass', function () {
 	return gulp.src('./assets/stylesheets/site.scss')
 	.pipe(sass({
 		outputStyle: 'compressed'
@@ -24,22 +22,12 @@ gulp.task('sass', ['clean'], function () {
 	.pipe(gulp.dest('./public'));
 });
 
-gulp.task('js', ['clean'], function () {
-	return gulp.src('./assets/javascripts/**/*.js')
-	.pipe(concat('site.js'))
-	.pipe(rev())
-	.pipe(gulp.dest('./public'));
-	// return gulp.src('./assets/javascripts/**/*.js')
-	// .pipe(uglify())
-	// .pipe(gulp.dest('./public'));
-});
-
-gulp.task('moveAssets', ['sass', 'js'], function () {
+gulp.task('moveAssets', function () {
 	gulp.src('./assets/fonts/**/*.*')
 	.pipe(gulp.dest('./public/fonts'));
 
 	gulp.src('./assets/images/**/*.*')
-	.pipe(gulp.dest('./public/images'));
+	.pipe(gulp.dest('./public/images/'));
 
 	gulp.src('./assets/public/**/*.*')
 	.pipe(gulp.dest('./public'));
@@ -47,13 +35,13 @@ gulp.task('moveAssets', ['sass', 'js'], function () {
 	return gulp.src('./index.html').pipe(gulp.dest('./public'));
 });
 
-gulp.task('index', ['moveAssets'], function () {
+gulp.task('index', function () {
 	var target = gulp.src('./public/index.html');
 	var sources = gulp.src(['./public/**/*.css', './public/**/*.js'], { read: false });
 
 	return target.pipe(inject(sources, { relative: true })).pipe(gulp.dest('./public/'));
 });
 
-gulp.task('default', ['index'], function () {
-
+gulp.task('default', function (callback) {
+	runSequence('clean', 'sass', 'moveAssets', 'index', callback);
 });
