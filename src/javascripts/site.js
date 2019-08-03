5b2848c06e77adcd8ca676dacdf55a7465333153
Ivan Bozic <ivan@arsfutura.co>
2019-08-03 08:31:13 +0200

    Added support for automatic dark mode between 10 PM and 6 AM.

--- src/javascripts/site.js
+++ src/javascripts/site.js
@@ -1,3 +1,18 @@
+function setDarkMode() {
+    console.log("asd");
+    document.getElementsByTagName("body")[0].dataset.theme = "dark";
+}
+
+function isItNighttime() {
+    var currentHour = new Date().getHours();
+
+    return currentHour < 6 || currentHour > 22 ? true : false;
+}
+
+if (isItNighttime() == true) setDarkMode();
+
+// Tapping on "Show more" in the reading section will show
+// the hidden list elements and hide the "Show more" button.
 document.addEventListener("click", function(event) {
     if (event.target.dataset.action == "reading-show-more") {
         var elements = document.querySelectorAll("li.hidden");
