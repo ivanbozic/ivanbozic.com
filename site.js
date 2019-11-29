2e88dc2bea49ee4b7ea7115736c51910e51a61c2
Ivan Bozic <ivan@arsfutura.co>
2019-11-29 19:31:29 +0100

    Switched to IBM Plex Mono. Added some more books.

--- site.js
+++ site.js
@@ -1,5 +1,4 @@
 function setDarkMode() {
-    console.log("asd");
     document.getElementsByTagName("body")[0].dataset.theme = "dark";
 }
 
@@ -10,15 +9,3 @@ function isItNighttime() {
 }
 
 if (isItNighttime() == true) setDarkMode();
-
-// Tapping on "Show more" in the reading section will show
-// the hidden list elements and hide the "Show more" button.
-document.addEventListener("click", function(event) {
-    if (event.target.dataset.action == "reading-show-more") {
-        var elements = document.querySelectorAll("li.hidden");
-        Array.prototype.forEach.call(elements, function(el, i) {
-            el.classList.remove("hidden");
-        });
-        event.target.style.display = "none";
-    }
-});
